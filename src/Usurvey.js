5ade1d6c905063606586e93652ece7b542587973
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-09-21 14:34:13 +0100

    Create Usurvey component and call it into the App component

---
+++ src/Usurvey.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class Usurvey extends Component {
+  render() {
+    return (
+      <div>
+        <p>I am from Usurvey component</p>
+      </div>
+    )
+  }
+}
+
+export default Usurvey
