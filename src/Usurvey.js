caec60c6127c737056671d06d8ab7b0b1c0d3ef8
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-09-21 14:57:39 +0100

    initialize the firebase configuration andn create the Usurvey constructor and the default condition fro the login

--- src/Usurvey.js
+++ src/Usurvey.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react'
+var firebase = require('firebase');
+var uuid = require('uuid');
+
+var config = {
+  apiKey: "AIzaSyBoxJbIpB9h9emvpDR95RTpIKISqmfjPBQ",
+  authDomain: "uservey-75e47.firebaseapp.com",
+  databaseURL: "https://uservey-75e47.firebaseio.com",
+  projectId: "uservey-75e47",
+  storageBucket: "uservey-75e47.appspot.com",
+  messagingSenderId: "896643048986"
+};
+firebase.initializeApp(config);
 
  class Usurvey extends Component {
+   constructor(prop) {
+     super(prop);
+     
+     this.state= {
+       uid: uuid.v1(),
+       studentName: '',
+       answers: {
+         answer1: '',
+         answer2: '',
+         answer3: ''
+       },
+       isSubmitted: false
+     };
+   }
+   
   render() {
+    var studentName;
+    var questions;
+
+    if(this.state.studentName === '' && this.state.isSubmitted === false){
+      studentName = <div>
+        <h1>Hey Student, please let us know your name:</h1>
+        <form>
+          <input type='text' placeholder='Enter your name' ref='name'></input>
+        </form>
+      </div>
+    }
     return (
       <div>
-        <p>I am from Usurvey component</p>
+        {studentName}
+        -----------------------------------
+        {questions}
       </div>
     )
   }
